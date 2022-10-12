e62e571fa0e38bbb4c14648404d30ffdf09ec0a5
Arslan Malik <arslan@shubbaktech.com>
2022-10-12 18:13:58 +0400

    d

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {DragDropModule} from '@angular/cdk/drag-drop';
 import { HomeComponent } from 'src/pages/home/home.component';
+import {MatSelectModule} from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule, MatInputModule } from '@angular/material';
 @NgModule({
   declarations: [
     AppComponent,
@@ -13,7 +16,11 @@ import { HomeComponent } from 'src/pages/home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    DragDropModule
+    DragDropModule,
+    MatSelectModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule
 
   ],
   providers: [],
---
+++ src/pages/home/common/data.ts
@@ -0,0 +1,71 @@
+
+ export const Seats = [
+      {
+          name:"Row1",
+          numberOfSeat:4,
+          seat:[
+          ]
+      },
+      {
+          name:"Row2",
+          numberOfSeat:4,
+          seat:[
+          ]
+      }
+  ]
+
+  export const Test_Ticket = [
+    {
+        name:"Category1",
+        numberOfTicket:4,
+        tickets:[
+        ]
+    },
+    {
+        name:"Category2",
+        numberOfTicket:4,
+        tickets:[
+        ]
+    },
+    {
+      name:"Category3",
+      numberOfTicket:4,
+      tickets:[
+      ]
+  },
+  {
+    name:"Category4",
+    numberOfTicket:4,
+    tickets:[
+    ]
+}
+]
+  export const Tickets = [
+    {
+      id: 1,
+      item: 'Ticket1'
+    },
+
+    {
+      id: 2,
+      item: 'Ticket2'
+    },
+    {
+      id: 3,
+      item: 'Ticket3'
+    },
+
+    {
+      id: 4,
+      item: 'Ticket4'
+    },
+    {
+      id: 5,
+      item: 'Ticket5'
+    },
+
+    {
+      id: 6,
+      item: 'Ticket6'
+    },
+  ]
